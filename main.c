52ee865795d1d48c25c3d4531c59c6cf1a93e724
0meeee0 <mehdi74.id@gmail.com>
2023-09-21 17:19:00 +0100

    taghyirat

--- main.c
+++ main.c
@@ -8,18 +8,76 @@ struct tache{
     int dl;
     char status[20];
 };
-
+int j = 0;
+struct tache add[1000];
 void ajoute(){
-//    int i;
-//    struct tache add[i];
+
     printf("\033[0;33m***********|Ajouter des nouvelles taches|**********\n\n\033[0m");
 
+int k;
+        do{
+            printf("|------------------task ID : %d-------------------|\n", j + 1);
+            printf("ash atsmiha : ");
+            add[j].id = j + 1 ;
+            scanf(" %[^\n]", add[j].titre);
+            printf("kidayra : ");
+            scanf(" %[^\n]", add[j].status);
+            printf("wsfha : ");
+            scanf(" %[^\n]", add[j].des);
+            printf("imta atsali: ");
+            scanf("%d", &add[j].dl);
+            printf("-task        : %s\n", add[j].titre);
+            printf("-status      : %s\n", add[j].status);
+            printf("-description : %s\n", add[j].des);
+            printf("-deadline    : %d\n", add[j].dl);
+            j++;
 
-    printf("Combien de tache ?");
+            printf("kml wla trjl: ah  1 la 0");
+            scanf("%d",&k);
+        }while(k);
 
 }
+
 void modif(){
+    int zin, mod ;
     printf("\033[0;33m***********|Modifier une tache|**********\n\n\033[0m");
+
+    printf("tmodifi chi 7aja ?\n");
+    printf("ktab l ID dial dakshi libaghi tbadal");
+    scanf("%d",&zin);
+    for (int x = 0 ; x < j; x++){
+        if(add[x].id == zin){
+            printf("mitsukitayo \n");
+            printf("-task        : %s\n", add[x].titre);
+            printf("-status      : %s\n", add[x].status);
+            printf("-description : %s\n", add[x].des);
+            printf("-deadline    : %d\n", add[x].dl);
+
+            do{
+                    printf("\033[0;31m*****  MODIFICATION  *****\n\n");
+            printf("\033[0;32m");// green
+
+            printf("1- Mofier la discripitiob.\n");
+            printf("2- Modifier status.\n");
+            printf("3- Modifier le deadline \n");
+            printf("0- Malk ?\n");
+
+            printf("\033[0m-----------------------------------------");
+            scanf("%d",&mod);
+            switch(mod){
+            case 1:
+                printf("good");
+                break;
+
+            }
+
+            }while (mod);
+        }
+        else
+            printf("la \n");
+    }
+
+
 }
 
 void suppr(){
@@ -46,15 +104,17 @@ void menu(){
     printf("3- Supprimer une tache par identifiant.\n");
     printf("4- Rechercher des taches.\n");
     printf("5- Statestiques.\n");
+    printf("0- Malk ?\n");
 
     printf("\033[0m-----------------------------------------");  // white
 }
 
 int main(){
     int i;
-        menu();
-        printf("\nEntrer une du 5 options\n");
+
     do{
+            menu();
+        printf("\nEntrer une du 5 options\n");
         scanf("%d",&i);
 
         switch (i){
@@ -74,7 +134,7 @@ int main(){
                 stats();
                 break;
             default :
-                printf("shi wa7d fihom");
+                printf("mamalish bn8");
         }
     }while(i);
     /*if (i < 1 || i > 5)
